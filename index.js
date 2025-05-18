8192afcfe9c2caaea4834fae3dd2e7806962953b
mskim9097 <mskim9097@gmail.com>
2025-05-18 09:39:12 -0700

    final assignment3

--- index.js
+++ index.js
@@ -1,34 +1,208 @@
+let clickCount = 0;
+let matchedPairs = 0;
+let totalPairs = 3;
+let timer = 30;
+let timerInterval = null;
+let gameOver = false;
 
+async function loadPokemonPairs(totalPairsInput, timerInput) {
+    clearInterval(timerInterval);
+    clickCount = 0;
+    matchedPairs = 0;
+    gameOver = false;
+    timer = timerInput;
+    totalPairs = totalPairsInput;
+    updateStatus();
 
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`);
+    let jsonObj = await response.json();
+    let allPokemon = jsonObj.results;
 
-function setup () {
-  let firstCard = undefined
-  let secondCard = undefined
-  $(".card").on(("click"), function () {
-    $(this).toggleClass("flip");
-
-    if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
-    else {
-      secondCard = $(this).find(".front_face")[0]
-      console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      ) {
-        console.log("match")
-        $(`#${firstCard.id}`).parent().off("click")
-        $(`#${secondCard.id}`).parent().off("click")
-      } else {
-        console.log("no match")
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
-        }, 1000)
-      }
+    let selected = new Set();
+    while (selected.size < totalPairsInput) {
+        let randomIndex = Math.floor(Math.random() * allPokemon.length);
+        selected.add(allPokemon[randomIndex].name);
     }
-  });
+
+    let pairs = [...selected, ...selected];
+    pairs.sort(() => Math.random() - 0.5);
+
+    let gameGrid = document.getElementById("game_grid");
+    gameGrid.innerHTML = "";
+
+    let idCounter = 1;
+
+    for (let name of pairs) {
+        const pokeRes = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokeData = await pokeRes.json();
+        const imgSrc = pokeData.sprites.other["official-artwork"].front_default;
+
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <img id="img${idCounter}" class="front_face" src="${imgSrc}" alt="${name}">
+        <img class="back_face" src="back.webp" alt="back">
+        `;
+        gameGrid.appendChild(card);
+        idCounter++;
+    }
+    setup();
+    applyGridLayout(totalPairsInput);
+    startTimer();
 }
 
-$(document).ready(setup)
+function setup() {
+    let firstCard = undefined;
+    let secondCard = undefined;
+    let isFlipping = false;
+
+    $(".card").on(("click"), function () {
+        if (gameOver) return;
+        if (isFlipping) return;
+        if ($(this).hasClass("flip")) return;
+        $(this).toggleClass("flip");
+
+        clickCount++;
+        updateStatus();
+        if (!firstCard) {
+            isFlipping = true;
+            firstCard = $(this).find(".front_face")[0];
+            $(this).one("transitionend", () => {
+                isFlipping = false;
+            });
+        }
+        else {
+            isFlipping = true;
+            secondCard = $(this).find(".front_face")[0];
+            if (firstCard === secondCard) return;
+
+            if (firstCard.src == secondCard.src) {
+                $(`#${firstCard.id}`).parent().off("click");
+                $(`#${secondCard.id}`).parent().off("click");
+                $(`#${firstCard.id}`).parent().addClass("matched");
+                $(`#${secondCard.id}`).parent().addClass("matched");
+
+                matchedPairs++;
+                updateStatus();
+                if (matchedPairs === totalPairs) {
+                    clearInterval(timerInterval);
+                    document.getElementById("status_message").textContent = "🎉 You Win!";
+                }
+
+                firstCard = undefined;
+                secondCard = undefined;
+                isFlipping = false;
+            } else {
+                setTimeout(() => {
+                    $(`#${firstCard.id}`).parent().toggleClass("flip");
+                    $(`#${secondCard.id}`).parent().toggleClass("flip");
+
+                    firstCard = undefined;
+                    secondCard = undefined;
+                    isFlipping = false;
+                }, 1000)
+            }
+
+        }
+    });
+}
+
+function updateStatus() {
+    document.getElementById("clicks").textContent = clickCount;
+    document.getElementById("matched").textContent = matchedPairs;
+    document.getElementById("left").textContent = totalPairs - matchedPairs;
+    document.getElementById("total").textContent = totalPairs;
+}
+
+function startTimer() {
+    document.getElementById("timer").textContent = timer;
+
+    timerInterval = setInterval(() => {
+        timer--;
+
+        document.getElementById("timer").textContent = timer;
+
+        if (timer <= 0) {
+            clearInterval(timerInterval);
+            gameOver = true;
+            document.getElementById("status_message").textContent = "⏰ Game Over!";
+            $(".card").off("click");
+        }
+    }, 1000);
+}
+
+document.getElementById("start_button").addEventListener("click", () => {
+    document.getElementById("status_message").textContent = "";
+    document.getElementById("start_button").style.display = "none";
+    document.getElementById("powerup_button").style.display = "inline";
+    const settings = getDifficultySettings();
+    loadPokemonPairs(settings.pairs, settings.time);
+});
+
+document.getElementById("reset_button").addEventListener("click", () => {
+    clickCount = 0;
+    matchedPairs = 0;
+    timer = 30;
+    gameOver = false;
+    clearInterval(timerInterval);
+
+    document.getElementById("status_message").textContent = "";
+    updateStatus();
+    document.getElementById("start_button").style.display = "inline";
+    document.getElementById("powerup_button").style.display = "inline";
+    loadPokemonPairs();
+});
+
+function getDifficultySettings() {
+    const difficulty = document.getElementById("difficulty").value;
+
+    switch (difficulty) {
+        case "easy":
+            return { pairs: 3, time: 30 };
+        case "medium":
+            return { pairs: 6, time: 60 };
+        case "hard":
+            return { pairs: 10, time: 120 };
+    }
+}
+document.getElementById("light_mode_button").addEventListener("click", () => {
+    document.getElementById("game_grid").style.backgroundColor = "white";
+});
+
+document.getElementById("dark_mode_button").addEventListener("click", () => {
+    document.getElementById("game_grid").style.backgroundColor = "black";
+});
+
+function applyGridLayout(pairs) {
+    const grid = document.getElementById("game_grid");
+    const cards = document.querySelectorAll(".card");
+
+    if (pairs === 3) {
+        grid.style.width = "600px";
+        grid.style.height = "400px"
+        cards.forEach(card => card.style.width = "33.33%");
+    } else if (pairs === 6) {
+        grid.style.width = "800px";
+        grid.style.height = "600px";
+        cards.forEach(card => card.style.width = "25%");
+    } else if (pairs === 10) {
+        grid.style.width = "1000px";
+        grid.style.height = "800px";
+        cards.forEach(card => card.style.width = "20%");
+    }
+}
+
+document.getElementById("powerup_button").addEventListener("click", () => {
+    if (gameOver) return;
+    document.getElementById("powerup_button").style.display = "none";
+    document.querySelectorAll(".card").forEach(card => {
+        card.classList.add("flip");
+    });
+    setTimeout(() => {
+        document.querySelectorAll(".card").forEach(card => {
+            if (!card.classList.contains("matched")) {
+                card.classList.remove("flip");
+            }
+        });
+    }, 5000);
+});
